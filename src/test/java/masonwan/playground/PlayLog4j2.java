e6b67cb2ea7f3afcef0e968584f4827a2026c9b9
Mason Wan <menghsiw@amazon.com>
2016-06-09 23:42:42 -0700

    Add PlayLog4j2

---
+++ src/test/java/masonwan/playground/PlayLog4j2.java
@@ -0,0 +1,19 @@
+package masonwan.playground;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.testng.annotations.Test;
+
+@Log4j2
+public class PlayLog4j2 {
+    @Test
+    public void test_setRootLogLevel() throws Exception {
+        Configurator.setRootLevel(Level.DEBUG);
+
+        log.debug("Debug");
+        log.info("Info");
+        log.warn("Warn");
+        log.error("Error");
+    }
+}
