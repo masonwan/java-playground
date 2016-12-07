587a697164d6a74c36633e21a876da3e2a48b841
Mason Wan <mwan@linkedin.com>
2016-12-07 15:44:39 -0800

    Add stream reduce test case

--- src/test/java/masonwan/playground/PlayStream.java
+++ src/test/java/masonwan/playground/PlayStream.java
@@ -3,15 +3,16 @@ package masonwan.playground;
 import com.google.common.collect.Lists;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayStream {
     @Test
-    public void testToList() throws Exception {
+    public void test_list() throws Exception {
         List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
 
         List<Integer> newList = list.stream()
@@ -24,7 +25,7 @@ public class PlayStream {
     }
 
     @Test
-    public void testToMap() throws Exception {
+    public void test_toMap() throws Exception {
         List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
         Map<Integer, Integer> map = list.stream()
             .map((num) -> num * 2)
@@ -35,6 +36,28 @@ public class PlayStream {
             int key = num.getKey();
             int value = num.getValue();
             assertThat(value == key * 2).isTrue();
+        });
+    }
+
+    @Test
+    public void test_reduce() throws Exception {
+        List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
+        Map<Integer, Integer> result = list.stream()
+            .<Map<Integer, Integer>>reduce(
+                new HashMap<>(),
+                (map, a) -> {
+                    map.put(a, a * 2);
+                    return map;
+                },
+                (map1, map2) -> {
+                    map1.putAll(map2);
+                    return map1;
+                }
+            );
+
+        assertThat(result.size()).isEqualTo(list.size());
+        result.forEach((v1, v2) -> {
+            assertThat(v1 * 2 == v2).isTrue();
         });
     }
 }
